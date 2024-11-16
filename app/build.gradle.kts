58d0a073b1d5c0634033d7d729a62825461db5df
Yurii Balan <ybalan19@gmail.com>
2024-11-16 21:56:43 +0200

    v1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.chargercharts2"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0"
+        versionCode = 0
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
